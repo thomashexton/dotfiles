206c6921c36436bddf097d54542c624aff4ffa44
Thomas Hexton <thomas.hexton@oneflare.com>
2020-10-08 10:35:52 +1100

    fourth sync

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -21,6 +21,7 @@ alias gcpr="git checkout production"
 alias rollback-site="kubectl rollout undo deployment/site -n site"
 alias rollback-graphql="kubectl rollout undo deployment/graphql -n site"
 alias kstage='asdf shell kubectl 1.15.11 && asp staging && aws eks update-kubeconfig --name=staging --alias=staging && kubectx staging'
+alias kprod='asdf shell kubectl 1.8.6 && asp production && kubectx production'
 
 # Rails
 alias be="bundle exec"
@@ -32,8 +33,9 @@ alias dbmigrate="bundle exec rake db:migrate"
 alias dbrollback="bundle exec rake db:rollback"
 alias db:reset="bundle exec rake db:reset"
 alias db:reset:test="bundle exec rake db:reset RAILS_ENV=test"
-# alias rspec="bundle exec rspec"
+alias rspec="bundle exec rspec"
 alias kill-everything="pkill -9 -f 'rb-fsevent|rails|spring|puma'"
+alias gqlschema="bundle exec rake graphql:dump_schema"
 
 # Finder Shortcuts
 alias cdsite="cd ~/Oneflare/Site"
@@ -45,10 +47,9 @@ alias womo="cd ~/Oneflare/womo"
 
 alias ic='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs'
 
-# Switch kube clusters
-alias kprod='asdf shell kubectl 1.8.6 && kubectx production'
+# Displayplacer Setups
+# # Switch Res to 1080p
+alias hd1080='displayplacer "id:120F7F58-74F0-A671-FAC3-4A43FC492AC8 res:1920x1080 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0" "id:8F203084-8EC4-5D28-9618-86A96CAA2871 res:1080x1920 hz:120 color_depth:8 scaling:off origin:(1920,-420) degree:270"'
 
-# Switch Res to 1080p
-alias 2x1080='displayplacer "id:120F7F58-74F0-A671-FAC3-4A43FC492AC8 res:1920x1080 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0" "id:5AC64EBE-6F82-D422-C0BA-8C22649B36FC res:1920x1080 hz:60 color_depth:8 scaling:on origin:(-1920,0) degree:0"'
-# Switch Res to 1440p
-alias 2x1440='displayplacer "id:120F7F58-74F0-A671-FAC3-4A43FC492AC8 res:2560x1440 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0" "id:5AC64EBE-6F82-D422-C0BA-8C22649B36FC res:2560x1440 hz:60 color_depth:8 scaling:on origin:(-2560,0) degree:0"'
+# # Switch Res to 1440p
+alias hd1440='displayplacer "id:120F7F58-74F0-A671-FAC3-4A43FC492AC8 res:2560x1440 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0" "id:8F203084-8EC4-5D28-9618-86A96CAA2871 res:1080x1920 hz:120 color_depth:8 scaling:off origin:(1920,-240) degree:270"'
