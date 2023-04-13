631a6afabf676ad109c66fc0548f8a7984709b76
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2023-04-13 12:03:16 +1000

    update ssh keys to ed25519, adjust ssh config file

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,45 +1,29 @@
-# Homebrew
-alias ibrew="arch -x86_64 /usr/local/bin/brew"
-alias remove_postgres_pid="rm -f $HOMEBREW_PREFIX/var/postgres/postmaster.pid"
-
 # Rails
-alias be="bundle exec"
-alias rs="bundle exec rails s"
-alias rc="bundle exec rails c"
-alias db:reset="bundle exec rake db:reset"
-alias db:reset:test="bundle exec rake db:reset RAILS_ENV=test"
-alias db:migrate="bundle exec rake db:migrate"
-alias db:migrate:test="bundle exec rake db:migrate RAILS_ENV=test"
-alias db:rollback="bundle exec rake db:rollback"
-alias db:rollback:test="bundle exec rake db:rollback RAILS_ENV=test"
-alias rspec="bundle exec rspec"
-alias wpds="./bin/webpack-dev-server"
-
-# Git
-alias gcfo="git config --list --show-origin"
-alias gfd="git diff --name-status master.."
-
-# Nicely formatted diff for announcements
-# alias deploydiff="git log main..staging --pretty=format:"%<(23)%an    %s" --abbrev-commit"
+alias be="bundle exec --quiet"
+alias rs="be rails s"
+alias rc="be rails c"
+alias db:reset="be rake db:reset"
+alias db:reset:test="be rake db:reset RAILS_ENV=test"
+alias db:migrate="be rake db:migrate"
+alias db:migrate:test="be rake db:migrate RAILS_ENV=test"
+alias db:rollback="be rake db:rollback"
+alias db:rollback:test="be rake db:rollback RAILS_ENV=test"
+alias rspec="be rspec"
 
 # iCloud dir
 alias ic="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs"
 
-# Edit hosts file
-alias hosts="sudo $VISUAL_EDITOR /etc/hosts"
-
-# GC Start
 alias e="$EDITOR"
 alias ve="$VISUAL_EDITOR"
 
 # Use nvim instead of vim/vi
 alias vim="nvim"
 alias vi="nvim"
-alias yab="vim ~/.yabairc"
-alias skh="vim ~/.skhdrc"
 
-# This is required so you can use aliases with "watch"
-alias watch="watch "
+# Quick open files
+alias hosts="sudo $VISUAL_EDITOR /etc/hosts"
+alias yab="nvim ~/.yabairc"
+alias skh="nvim ~/.skhdrc"
 
 # Show the weather
 alias wx="curl v2.wttr.in"
@@ -48,4 +32,4 @@ alias wx="curl v2.wttr.in"
 alias pubip="curl -s ipv4.icanhazip.com | tee >(pbcopy)"
 
 # Copy my public SSH rsa to clipboard
-alias pubssh="pbcopy < ~/.ssh/id_rsa.pub"
+alias pubssh="pbcopy < ~/.ssh/personal_laptop_ed25519.pub"
