ac49e6c22944b3e67d7ad6cc3ef4c46777bf0fbc
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2025-04-23 12:34:24 +1000

    adding fish, karabiner, linearmouse and stow configs to stow_configs fn

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,7 @@ set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
 
 set -Ux EDITOR nvim # neovim
-set -Ux VISUAL_EDITOR zed # visual studio code
+set -Ux VISUAL_EDITOR zed # visual editor
 
 set -U fish_greeting # disable fish greeting
 set -U fish_key_bindings fish_vi_key_bindings
@@ -17,11 +17,3 @@ abbr bic "brew install --cask"
 abbr binf "brew info"
 abbr binfc "brew info --cask"
 abbr bs "brew search"
-
-# Yabai & SKHD
-abbr yab "nvim ~/.yabairc"
-abbr skh "nvim ~/.skhdrc"
-abbr yabsto "yabai --stop-service"
-abbr yabsta "yabai --start-service"
-
-starship init fish | source
