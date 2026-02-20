87e3c4d173a955de78de8840d25ada3a326a7bc0
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-02-20 11:29:12 +1100

    Overhaul tmux setup with Alacritty as binding layer
    - Switch tmux prefix back to Ctrl+B default
    - Add Alacritty bindings for Ctrl+T/W/K and Ctrl+1-9 as tmux shortcuts
    - Add tmux auto-start to environment.zsh
    - Add detach-on-destroy, focus-events quality of life options
    - Switch font to JetBrainsMono Nerd Font in Alacritty

--- zsh/.config/zsh/custom/environment.zsh
+++ zsh/.config/zsh/custom/environment.zsh
@@ -4,3 +4,8 @@ export ZSHZ_DATA="${XDG_CACHE_HOME}/.z"
 
 export EDITOR='zed --wait'
 export VISUAL_EDITOR='zed --wait'
+
+# Auto-attach or create a tmux session when opening a terminal
+if [ -z "$TMUX" ]; then
+  tmux new-session -A -s main
+fi
