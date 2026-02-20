fb671a15cd4ebc47f7e9b2cf9c9119b708acdd68
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-02-20 11:50:40 +1100

    Remove detach-on-destroy, update deps and misc config
    - Remove detach-on-destroy from tmux to allow clean shell exit
    - Update Brewfile dependencies
    - Update git config
    - Update zed settings
    - Clean up zsh functions

--- zsh/.config/zsh/custom/functions.zsh
+++ zsh/.config/zsh/custom/functions.zsh
@@ -1,25 +1,5 @@
-function print_wattage {
-  while true
-  do
-    local wattage=$(/usr/sbin/system_profiler SPPowerDataType | grep -E 'Wattage \(W\): (.*)' 2>/dev/null | awk '{print $3}')
-    if [[ -z "${wattage}" ]]; then
-      echo "no charger connected"
-    else
-      echo "current wattage: ${wattage}W"
-    fi
-    sleep 1
-  done
-}
-
 function reset_tracked_branches {
-    # Set the remote branches to track
-    git remote set-branches origin master green 'thomashexton-*' 'jayt-*' 'regina*'
-
-    # Fetch the updated branch references from the remote
-    git fetch --prune
-
-    # The --prune option above will delete any stale tracking branches
-    # that are no longer present on the remote. If you also want to delete
-    # local branches that are no longer tracking a remote branch, you can use:
-    git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -d
+    git remote set-branches origin master green 'thomashexton-*' 'jayt-*' 'regina*' &&
+    git fetch --prune &&
+    git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs --no-run-if-empty git branch -D
 }
