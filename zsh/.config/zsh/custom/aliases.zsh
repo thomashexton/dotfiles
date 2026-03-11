1699ea1a23d41f427cc39ce34642ed3caad14a27
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-03-12 10:16:10 +1100

    Update skhc alias path and remove pubssh

--- zsh/.config/zsh/custom/aliases.zsh
+++ zsh/.config/zsh/custom/aliases.zsh
@@ -10,15 +10,10 @@ alias vi="nvim"
 
 # Quick open files
 alias hosts="sudo ${VISUAL_EDITOR} /etc/hosts"
-alias skhc="nvim ~/.skhdrc"
+alias skhc="nvim ~/.config/skhd/skhdrc"
 
 # Show the weather
 alias wx="curl v2.wttr.in"
 
 # Copy public ip to the clipboard
 alias pubip="curl -s ipv4.icanhazip.com | tee >(pbcopy)"
-
-# Copy my public SSH rsa to clipboard
-alias pubssh="pbcopy < ~/.ssh/personal_laptop_ed25519.pub"
-
-# Work-specific aliases are in iCloud zsh secrets
